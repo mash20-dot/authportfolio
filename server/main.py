9ed1b536eb17b07f479f6e30343895b7f98de6e4
Sakyi Mustapha <sakyimustapha5@gmail.com>
2025-07-21 11:26:47 +0000

    added a gunicorn

--- server/main.py
+++ server/main.py
@@ -140,4 +140,5 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
